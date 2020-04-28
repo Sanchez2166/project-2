34f46fc149877b35ef8b9c4e6165dcbc3a9cf9ce
Noe Sanchez Lamas <nsl@uoregon.edu>
2020-04-27 22:52:46 -0700

    Project 2 Finished

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,5 @@
 // jshint esversion: 6
-
+/*
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
@@ -13,4 +13,42 @@ let containsDuplicates = s => {
     }
   }
   return false;
+};
+*/
+let revWord = str => {
+  let arr = str.split("");
+  let result = "";
+  for (let char of arr) {
+    result = char + result;
+  }
+  return result;
+};
+//3a
+let revWords1 = str => {
+  let arr = str.split(" ");
+  let result = "";
+  for (var i = 0; i < arr.length; i++) {
+    result = result + " " + revWord(arr[i]);
+  }
+  return result;
+};
+
+//3b
+let revWords2 = str => {
+  let arr = str.split(" ");
+  let result = "";
+  arr.forEach((item, i) => {
+    result = result + " " + revWord(arr[i]);
+  });
+  return result;
+};
+
+//3c
+let revWords3 = str => {
+  let arr = str.split(" ");
+  let result = "";
+  for (let i of arr) {
+    result = result + " " + revWord(i);
+  }
+  return result;
 };
